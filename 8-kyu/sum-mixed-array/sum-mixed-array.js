621f576c75730b062bc022cb003182e4f20f9c5d
Albert Reyes <reyesenterprise94@gmail.com>
2025-03-14 20:52:42 -0700

    Update solution - CodeHub

--- 8-kyu/sum-mixed-array/sum-mixed-array.js
+++ 8-kyu/sum-mixed-array/sum-mixed-array.js
@@ -1,3 +1,9 @@
 function sumMix(x) {
-    return x.reduce((sum, item) => sum + Number(item), 0);
+    let sum = 0;
+    x.forEach(item => {
+        console.log(`Item: ${item}, Type: ${typeof item}, Converted: ${Number(item)}`);
+        sum += Number(item);
+    });
+    console.log(`Final Sum: ${sum}, Type: ${typeof sum}`);
+    return sum;
 }
